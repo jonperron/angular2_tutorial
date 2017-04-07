1b584e9385a7afea7e0fb6836ce2dcf5f12f218b
Jonathan Perron <nir@syllage.fr>
2017-04-07 15:37:31 +0200

    Includes routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+   <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
 }
